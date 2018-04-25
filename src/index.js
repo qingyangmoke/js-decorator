21c678c2005f1f3c94a10d69cb8f02b2f8ea35f2
采东 <fusheng.sfs@antfin.com>
2018-04-25 21:00:59 +0800

    1.0.1

--- src/index.js
+++ src/index.js
@@ -9,72 +9,70 @@ function Decorator(object, target) {
   this.target = target;
 }
 
-/**
- * 原始对象实例
- * @param {Object} context - 上下文
- */
-Decorator.prototype.getTarget = function (context) {
-  return (typeof this.target === 'function')
-    ? this.target.call(context)
-    : context[this.target];
-};
-
-/**
- * 装饰方法
- * @param {String} name - target中的方法名称
- * @param {String} newName - 映射到object的方法名
- * @return {Decorator} 对象本身链式调用
- */
-Decorator.prototype.method = function (name, newName) {
-  newName = newName || name;
-  const _this = this;
-  _this.object[newName] = function () {
-    const target = _this.getTarget(this);
-    return target[name].apply(target, arguments);
-  };
-  return _this;
-};
-
-/**
- * 同时 getter 和 setter
- * @param {String} name - target中的属性名称
- * @param {String} newName - 映射到object的属性名
- * @return {Decorator} 对象本身链式调用
- */
-Decorator.prototype.property = function (name, newName) {
-  this.getter(name, newName);
-  this.setter(name, newName);
-  return this;
-};
-
-/**
- * getter
- * @param {String} name - target中的属性名称
- * @param {String} newName - 映射到object的属性名
- * @return {Decorator} 对象本身链式调用
- */
-Decorator.prototype.getter = function (name, newName) {
-  newName = newName || name;
-  const _this = this;
-  _this.object.__defineGetter__(newName, function () {
-    return _this.getTarget(this)[name];
-  });
-  return _this;
-};
-
-/**
- * setter
- * @param {String} name - target中的属性名称
- * @param {String} newName - 映射到object的属性名
- * @return {Decorator} 对象本身链式调用
- */
-Decorator.prototype.setter = function (name, newName) {
-  newName = newName || name;
-  const _this = this;
-  _this.object.__defineSetter__(newName, function (val) {
-    _this.getTarget(this)[name] = val;
-  });
-  return _this;
+Decorator.prototype = {
+  /**
+   * 原始对象实例
+   * @param {Object} context - 上下文
+   */
+  _getTarget(context) {
+    return (typeof this.target === 'function')
+      ? this.target.call(context)
+      : context[this.target];
+  },
+  /**
+   * 装饰方法
+   * @param {String} name - target中的方法名称
+   * @param {String} newName - 映射到object的方法名
+   * @return {Decorator} 对象本身链式调用
+   */
+  method(name, newName) {
+    newName = newName || name;
+    const _this = this;
+    _this.object[newName] = function () {
+      const target = _this._getTarget(this);
+      return target[name].apply(target, arguments);
+    };
+    return _this;
+  },
+  /**
+   * 同时 getter 和 setter
+   * @param {String} name - target中的属性名称
+   * @param {String} newName - 映射到object的属性名
+   * @return {Decorator} 对象本身链式调用
+   */
+  property(name, newName) {
+    this.getter(name, newName);
+    this.setter(name, newName);
+    return this;
+  },
+  /**
+   * getter
+   * @param {String} name - target中的属性名称
+   * @param {String} newName - 映射到object的属性名
+   * @return {Decorator} 对象本身链式调用
+   */
+  getter(name, newName) {
+    newName = newName || name;
+    const _this = this;
+    _this.object.__defineGetter__(newName, function () {
+      return _this._getTarget(this)[name];
+    });
+    return _this;
+  },
+  /**
+   * setter
+   * @param {String} name - target中的属性名称
+   * @param {String} newName - 映射到object的属性名
+   * @return {Decorator} 对象本身链式调用
+   */
+  setter(name, newName) {
+    newName = newName || name;
+    const _this = this;
+    _this.object.__defineSetter__(newName, function (val) {
+      _this._getTarget(this)[name] = val;
+    });
+    return _this;
+  },
 };
 
 /**
